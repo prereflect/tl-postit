756290bac3a357c8730322022d39ef5a4c3b79d7
Toby Woller <tobywoller@gmail.com>
2015-01-20 16:13:15 -0800

    Assignment: User model
    create table users
    t.string :username

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150120233049) do
+ActiveRecord::Schema.define(version: 20150120235717) do
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
@@ -19,6 +19,12 @@ ActiveRecord::Schema.define(version: 20150120233049) do
     t.text     "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "username"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
