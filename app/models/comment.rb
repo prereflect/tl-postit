301ec5d856cfba6b37b256f88af024fa8cdecb75
Toby Woller <tobywoller@gmail.com>
2015-01-21 13:02:51 -0800

    belongs_to :creator, foreign_key: 'user_id'...
     belongs_to :creator, foreign_key: 'user_id',
                          class_name: 'User'
    
    Use :creator to be more explicit on who makes posts/comments

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,6 @@
 class Comment < ActiveRecord::Base
-  belongs_to :user
+  belongs_to :creator, foreign_key: 'user_id',
+                       class_name: 'User'
   belongs_to :post
 end
 
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,6 @@
 class Post < ActiveRecord::Base
-  belongs_to :user
+  belongs_to :creator, foreign_key: 'user_id',
+                       class_name: 'User'
   has_many :comments, dependent: :destroy
 end
 
