48d624e365ddf09043f500440de563091d7990a7
Toby Woller <tobywoller@gmail.com>
2015-01-20 16:30:10 -0800

    Assignment: 1:M between User and Post
    Add user_id column to posts table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150120235717) do
+ActiveRecord::Schema.define(version: 20150121001700) do
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20150120235717) do
     t.text     "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.string   "user_id"
   end
 
   create_table "users", force: :cascade do |t|
