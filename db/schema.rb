1563c9ff8710365cf79102c3da3443dd3a1e801e
Toby Woller <tobywoller@gmail.com>
2015-01-21 16:32:36 -0800

    Add Index to Foreign Keys

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150121035208) do
+ActiveRecord::Schema.define(version: 20150122002539) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
@@ -27,6 +27,9 @@ ActiveRecord::Schema.define(version: 20150121035208) do
     t.datetime "updated_at", null: false
   end
 
+  add_index "comments", ["post_id"], name: "index_comments_on_post_id"
+  add_index "comments", ["user_id"], name: "index_comments_on_user_id"
+
   create_table "posts", force: :cascade do |t|
     t.string   "title"
     t.string   "url"
@@ -35,6 +38,8 @@ ActiveRecord::Schema.define(version: 20150121035208) do
     t.datetime "updated_at",  null: false
     t.integer  "user_id"
   end
+
+  add_index "posts", ["user_id"], name: "index_posts_on_user_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "username"
