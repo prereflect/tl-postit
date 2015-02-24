a737e9502c3424dcc968b7856ce2c778f315e049
Toby Woller <tobywoller@gmail.com>
2015-02-24 10:51:26 -0800

    Delete display_datetime from helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,8 +2,4 @@ module ApplicationHelper
   def fix_url(str)
     str.starts_with?('http://') ? str : "http://#{str}"
   end
-
-  def display_datetime(dt)
-    dt.strftime("%m/%d/%Y <span class='quiet'>at</span> %l:%M%P %Z").html_safe
-  end
 end
