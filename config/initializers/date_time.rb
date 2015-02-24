0464abfcba4eb0564f7ff6e0939e5eb1cdf340a0
Toby Woller <tobywoller@gmail.com>
2015-02-24 10:21:51 -0800

    Format Date::DATE_FORMATS[:default]

---
+++ config/initializers/date_time.rb
@@ -0,0 +1,6 @@
+# Date
+Date::DATE_FORMATS[:default] = "%A, %e %B %Y"  # Tuesday, 24 February 2015
+
+# Time
+Time::DATE_FORMATS[:default] = "%A, %e %B %Y  at %l:%M%P %Z" # Tuesday, 24 February 2015 at 3:30pm UTC
+
