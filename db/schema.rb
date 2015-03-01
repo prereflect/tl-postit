3534f0bbc57449ccba7086c9ed852f33b1fa465e
Toby Woller <tobywoller@gmail.com>
2015-02-28 16:39:57 -0800

    Assignment: has_secure_password
    Add bcrypt gem
    Add `has_secure_password validations: false` to User model
    Add `password_digest` column to `users` table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150125013948) do
+ActiveRecord::Schema.define(version: 20150228235156) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
@@ -50,8 +50,9 @@ ActiveRecord::Schema.define(version: 20150125013948) do
 
   create_table "users", force: :cascade do |t|
     t.string   "username"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.string   "password_digest"
   end
 
 end
