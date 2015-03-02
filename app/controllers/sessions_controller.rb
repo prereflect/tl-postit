41ac6ac7f0dcacd3b614e1c57a375e57f8fec672
Toby Woller <tobywoller@gmail.com>
2015-03-02 15:52:50 -0800

    Add user validation, login/logout

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,23 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    user = User.find_by(username: params[:username])
+    
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      flash[:notice] = 'You have succesfully logged in!'
+      redirect_to root_path 
+    else
+      flash[:error] = 'Your username or password is incorrect.'
+      redirect_to register_url
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    flash[:notice] = 'You have succesfully logged out!'
+    redirect_to root_url
+  end
+end
