092e9ef1df7c14c0086ebfc0dcc29f496977332d
Toby Woller <tobywoller@gmail.com>
2015-03-04 16:16:26 -0800

    Add post_owner?

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  helper_method :current_user, :logged_in?
+  helper_method :current_user, :logged_in?, :post_owner?
 
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
@@ -11,6 +11,12 @@ class ApplicationController < ActionController::Base
 
   def logged_in?
     !!current_user
+  end
+
+  def post_owner?
+    if
+      session[:user_id] == @post.user_id
+    end
   end
 
   def require_user
