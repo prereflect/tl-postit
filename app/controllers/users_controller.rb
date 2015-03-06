7faa6f26d65c779a4c2ac6272284ca4c271055ff
Toby Woller <tobywoller@gmail.com>
2015-03-06 12:45:23 -0800

    Add immediate login for new user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
+      session[:user_id] = @user.id
       flash[:notice] = 'You are now registered'
       redirect_to root_url
     else
