81e13b0749d626e4a5559d97326803adf6dc8607
Toby Woller <tobywoller@gmail.com>
2015-03-07 03:05:45 -0800

    Add Timezone information to app and user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,12 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  before_filter :set_time_zone
+
+  def set_time_zone
+    Time.zone = current_user.time_zone if current_user
+  end
+
   helper_method :current_user, :logged_in?, :post_owner?
 
   def current_user
