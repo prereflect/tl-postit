b7d0a4293a88125fb96c2412ca78797ec02e3a9d
Toby Woller <tobywoller@gmail.com>
2015-03-11 17:01:09 -0700

    Change DateTime format

--- config/initializers/date_time.rb
+++ config/initializers/date_time.rb
@@ -1,6 +1,6 @@
 # Date
-Date::DATE_FORMATS[:default] = "%A, %e %B %Y"  # Tuesday, 24 February 2015
+Date::DATE_FORMATS[:default] = "%a, %e %b %Y"  # Tue, 3 Mar 2015
 
 # Time
-Time::DATE_FORMATS[:default] = "%A, %e %B %Y  at %l:%M%P %Z" # Tuesday, 24 February 2015 at 3:30pm UTC
+Time::DATE_FORMATS[:default] = "%a, %e %b %Y  at %l:%M%P %Z" # Tue, 3 Mar 2015 at 9:21pm PST
 
