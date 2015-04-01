f7574cd7785fcdf12feecce77f363676834905a0
Toby Woller <toby@woller.io>
2015-04-01 11:57:18 -0700

    Solution: Better Slugs
    Use various regex to allow only alphanumeric characters in
    Post title
    Category name
    Username

--- app/models/post.rb
+++ app/models/post.rb
@@ -9,7 +9,7 @@ class Post < ActiveRecord::Base
   validates :url, presence: true, uniqueness: true
   validates :description, presence: true
 
-  before_save :generate_slug
+  before_save :generate_slug!
 
   def total_votes
     up_votes - down_votes
@@ -27,7 +27,31 @@ class Post < ActiveRecord::Base
     self.slug
   end
 
-  def generate_slug
-    self.slug = self.title.gsub(' ', '-').downcase
+  def generate_slug!
+    the_slug = to_slug(self.title)
+    post = Post.find_by slug: the_slug
+    count = 2
+    while post && post != self
+      the_slug = append_suffix(the_slug, count)
+      post = Post.find_by slug: the_slug
+      count += 1
+    end
+    self.slug = the_slug
+  end
+
+  def to_slug(name)
+    str = name.strip
+    str.gsub! /\W/, '-'
+    str.gsub! /-+/, '-'
+    str.gsub! /_/, '-'
+    str.gsub! /^-+|-+$/, ''
+    str.downcase
+  end
+
+  def append_suffix(str, count)
+    if str.split('-').last.to_i != 0
+      return str.split('-').slice(0...-1).join('-') + '-' + count.to_s
+    end
+    return str + '-' + count.to_s
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -10,13 +10,38 @@ class User < ActiveRecord::Base
   validates :password, confirmation: true
   validates :password_confirmation, presence: true
 
-  before_save :generate_slug
+  before_save :generate_slug!
 
   def to_param
     self.slug
   end
-  def generate_slug
-    self.slug = self.username.gsub(' ', '-').downcase
+
+  def generate_slug!
+    the_slug = to_slug(self.username)
+    user = User.find_by slug: the_slug
+    count = 2
+    while user && user != self
+      the_slug = append_suffix(the_slug, count)
+      user = User.find_by slug: the_slug
+      count += 1
+    end
+    self.slug = the_slug
+  end
+
+  def to_slug(name)
+    str = name.strip
+    str.gsub! /\W/, '-'
+    str.gsub! /_/, '-'
+    str.gsub! /-+/, '-'
+    str.gsub! /^-|-$/, ''
+    str.downcase
+  end
+
+  def append_suffix(str, count)
+    if str.split('-').last.to_i != 0
+      return str.split('-').slice(0...-1).join('-') + '-' + count.to_s
+    end
+    return str + '-' + count.to_s
   end
 end
 
