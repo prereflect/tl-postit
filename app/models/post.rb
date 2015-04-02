9767fce83c2091bcc059ab5ce5de540379f85f75
Toby Woller <toby@woller.io>
2015-04-02 13:14:04 -0700

    Voteable module

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,6 @@
 class Post < ActiveRecord::Base
+  include Voteable
+
   belongs_to :creator, foreign_key: 'user_id', class_name: 'User'
   has_many :comments, dependent: :destroy
   has_many :post_categories
@@ -10,18 +12,6 @@ class Post < ActiveRecord::Base
   validates :description, presence: true
 
   before_save :generate_slug!
-
-  def total_votes
-    up_votes - down_votes
-  end
-
-  def up_votes
-    self.votes.where(vote: true).size
-  end
-
-  def down_votes
-    self.votes.where(vote: false).size
-  end
 
   def to_param
     self.slug
