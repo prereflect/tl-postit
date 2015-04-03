8a91f6563baeee257687c8bf00fa94af6ec1d3e3
Toby Woller <toby@woller.io>
2015-04-02 18:55:40 -0700

    Sluggable module

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
+  include Sluggable
+
   has_many :posts, dependent: :destroy
   has_many :comments, dependent: :destroy
   has_many :votes
@@ -10,38 +12,6 @@ class User < ActiveRecord::Base
   validates :password, confirmation: true
   validates :password_confirmation, presence: true
 
-  before_save :generate_slug!
-
-  def to_param
-    self.slug
-  end
-
-  def generate_slug!
-    the_slug = to_slug(self.username)
-    user = User.find_by slug: the_slug
-    count = 2
-    while user && user != self
-      the_slug = append_suffix(the_slug, count)
-      user = User.find_by slug: the_slug
-      count += 1
-    end
-    self.slug = the_slug
-  end
-
-  def to_slug(name)
-    str = name.strip
-    str.gsub! /\W/, '-'
-    str.gsub! /_/, '-'
-    str.gsub! /-+/, '-'
-    str.gsub! /^-|-$/, ''
-    str.downcase
-  end
-
-  def append_suffix(str, count)
-    if str.split('-').last.to_i != 0
-      return str.split('-').slice(0...-1).join('-') + '-' + count.to_s
-    end
-    return str + '-' + count.to_s
-  end
+  sluggable_column :username
 end
 
